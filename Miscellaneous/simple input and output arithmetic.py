25b6fcad845152ab8f4431faa4b3115b72c72a65
Adrian Yeung <yeungadd123@gmail.com>
2025-03-05 17:49:46 -0500

    Add files via upload

---
+++ Miscellaneous/simple input and output arithmetic.py
@@ -0,0 +1,16 @@
+var_a = int(input("Please input your first number: "))
+var_b = int(input("Please input your second number: "))
+
+# Integer addition
+print(var_a + var_b)
+
+# Integer subtraction
+print(var_a - var_b)
+
+# Integer multiplication
+print(var_a * var_b)
+
+# Floating-point division
+print(var_a / var_b)
+
+print("\nThat's all, folks!")
